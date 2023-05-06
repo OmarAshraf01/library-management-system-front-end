ef391a2d597fb70cc5a9f59e42fb0b17165944e6
Pubudu Janith <pubudujanith123@gmail.com>
2023-05-06 11:19:02 +0530

    Add page routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import SharedLayout from "./pages/SharedLayout";
+import Dashboard from "./pages/Dashboard";
+import ManageMembers from "./pages/ManageMembers";
+import ManageBooks from "./pages/ManageBooks";
+import IssueBooks from "./pages/IssueBooks";
+import HandleReturns from "./pages/HandleReturns";
 
 function App() {
     return (
-        <>
-
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<SharedLayout/>}>
+                    <Route index element={<Dashboard/>}/>
+                    <Route path={"manage-members"} element={<ManageMembers/>}/>
+                    <Route path={"manage-books"} element={<ManageBooks/>}/>
+                    <Route path={"issue-books"} element={<IssueBooks/>}/>
+                    <Route path={"handle-returns"} element={<HandleReturns/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,16 @@
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
 
 const Dashboard = () => {
     return (
         <>
-            <Box>Dashboard</Box>
+            <Box>
+                <Typography>Welcome to Library Management System</Typography>
+                <Link to={"/manage-members"}>Manage Members</Link>
+                <Link to={"/manage-books"}>Manage Books</Link>
+                <Link to={"/issue-books"}>Issue Books</Link>
+                <Link to={"/handle-returns"}>Handle Returns</Link>
+            </Box>
         </>
     )
 }
