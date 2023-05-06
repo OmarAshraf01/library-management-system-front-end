ab7463790a392aeb4683a938fd9afed394908511
Pubudu Janith <pubudujanith123@gmail.com>
2023-05-06 15:49:34 +0530

    Add stylings to Dashboard page layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import {Box, Grid, ThemeProvider, Typography} from "@mui/material";
+import {Box, Grid, List, ListItem, ListItemIcon, ListItemText, Typography} from "@mui/material";
 import {Link} from "react-router-dom";
 import Footer from "../components/Footer";
 import colorConfigs from "../configs/colorConfigs";
@@ -6,6 +6,7 @@ import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
 import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import SwipeUpOutlinedIcon from '@mui/icons-material/SwipeUpOutlined';
+import CircleIcon from '@mui/icons-material/Circle';
 
 const Dashboard = () => {
     return (
@@ -13,19 +14,29 @@ const Dashboard = () => {
             <Box
                 bgcolor={colorConfigs.mainBg}
                 minHeight={"100vh"}
-                paddingBottom={"70px"}
+                paddingBottom={"45px"}
             >
                 <Grid container pt={2} pb={3} pl={1} pr={1}>
                     <Grid item xs={12}>
                         <Typography
+                            sx={{
+                                textDecoration: "underline",
+                                color: "white",
+                                fontWeight: "bold"
+                            }}
                             variant={"h5"}
                             textAlign={"center"}
                         >
                             Welcome to Library Management System
                         </Typography>
                     </Grid>
+                    <Grid item xs={12}>
+                        <Typography pt={2} pl={2} pr={2} color={"white"} textAlign={"center"} fontStyle={"italic"}>
+                            A system that enables library administrator to manage members, manage books, manage issue book details and handle returns
+                        </Typography>
+                    </Grid>
                 </Grid>
-                <Grid pl={12} pr={12} container rowSpacing={4}>
+                <Grid pl={11} pr={11} container rowSpacing={4}>
                     <Grid item xs={12} sm={6} lg={3}>
                         <Box
                             display={"flex"}
@@ -39,6 +50,7 @@ const Dashboard = () => {
                                 to={"/manage-members"}
                             >
                                 <Box
+                                    id={"manage-members"}
                                     bgcolor={"white"}
                                     display={"flex"}
                                     justifyContent={"center"}
@@ -75,6 +87,7 @@ const Dashboard = () => {
                                 }}
                                 to={"/manage-books"}>
                                 <Box
+                                    id={"manage-books"}
                                     bgcolor={"white"}
                                     display={"flex"}
                                     justifyContent={"center"}
@@ -111,6 +124,7 @@ const Dashboard = () => {
                                 }}
                                 to={"/issue-books"}>
                                 <Box
+                                    id={"issue-books"}
                                     bgcolor={"white"}
                                     display={"flex"}
                                     justifyContent={"center"}
@@ -147,6 +161,7 @@ const Dashboard = () => {
                                 }}
                                 to={"/handle-returns"}>
                                 <Box
+                                    id={"handle-returns"}
                                     bgcolor={"white"}
                                     display={"flex"}
                                     justifyContent={"center"}
@@ -172,23 +187,81 @@ const Dashboard = () => {
                         </Box>
                     </Grid>
                 </Grid>
-                <Typography
+                <Box
                     pt={2} pl={5} pr={5}
                     textAlign={"justify"}
+                    color={"white"}
                 >
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At atque beatae cumque deserunt dolorum,
-                    error eum facere fugiat illum, incidunt, minus molestias natus nesciunt quia quos recusandae sequi
-                    unde? Aspernatur? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consectetur
-                    distinctio, error incidunt iure laboriosam minus quas repellat voluptas voluptate. Aperiam corporis
-                    dicta, dolores ducimus earum magnam officiis quae sit! Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Atque blanditiis debitis delectus dolor earum eligendi facilis, fugit, harum illum
-                    laboriosam laudantium molestias nihil omnis optio placeat quas ullam ut, vero! Lorem ipsum dolor sit
-                    amet, consectetur adipisicing elit. Aspernatur autem corporis dolor ex iure optio quaerat quisquam,
-                    sint. Assumenda ea earum error eveniet necessitatibus nulla qui rem sequi tenetur voluptatem! Lorem
-                    ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dolore eveniet in porro. Architecto
-                    at, officia! Doloremque harum incidunt itaque magnam rem vel veniam vero. Amet debitis dolorum omnis
-                    saepe.
-                </Typography>
+                    <Typography fontWeight={"bold"}>
+                        Highlighted features of the system,
+                    </Typography>
+                    <List dense={true}>
+                        <ListItem>
+                            <ListItemIcon>
+                                <CircleIcon sx={{fontSize: "10px", color: "white"}} />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary="Members are registered to the system by UUID, and no two members can have the same contact number."
+                            />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <CircleIcon sx={{fontSize: "10px", color: "white"}} />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary="Books are registered to the system by international standard book number (isbn)."
+                            />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <CircleIcon sx={{fontSize: "10px", color: "white"}} />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary="When members take books from the library they will receive an issue note."
+                            />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <CircleIcon sx={{fontSize: "10px", color: "white"}} />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary="The issue note contains all the take away book ISBNs along with the member UUID."
+                            />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <CircleIcon sx={{fontSize: "10px", color: "white"}} />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary="All issue notes have unique issue id to uniquely identify them when the books are returned."
+                            />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <CircleIcon sx={{fontSize: "10px", color: "white"}} />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary="Issue note can only have maximum 3 distinct ISBNs."
+                            />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <CircleIcon sx={{fontSize: "10px", color: "white"}} />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary="A member cannot take the same book from the system twice at the same time or at two different times (with another issue note)."
+                            />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <CircleIcon sx={{fontSize: "10px", color: "white"}} />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary="A member can only take maximum of 3 different books from the system. If he/she needs another, he/she must return a book that he/she already got."
+                            />
+                        </ListItem>
+                    </List>
+                </Box>
             </Box>
             <Footer/>
         </>
