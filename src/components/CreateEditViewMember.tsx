74bc10fc486ef60414f596d7ddff65f32de56a10
Pubudu Janith <pubudujanith123@gmail.com>
2023-05-07 19:37:04 +0530

    Implement Member validations

--- src/components/CreateEditViewMember.tsx
+++ src/components/CreateEditViewMember.tsx
@@ -1,4 +1,4 @@
-import React, {SetStateAction} from "react";
+import React, {ChangeEvent, SetStateAction, useState} from "react";
 import {Box, Button, Grid, IconButton, TextField, Typography} from "@mui/material";
 import CancelIcon from "@mui/icons-material/Cancel";
 
@@ -32,6 +32,13 @@ type ErrorMsgType = {
 }
 
 const CreateEditViewMember = ({mode, member, action} : Props) => {
+    const [newMember, setNewMember] = useState<Member>({
+        id: null, name: "", address: "", contact: ""
+    });
+    const [error, setError] = useState<ErrorMsgType>({
+        nameError: " ", addressError: " ", contactError: " "
+    })
+
     return (
         <>
             <Box
@@ -67,46 +74,118 @@ const CreateEditViewMember = ({mode, member, action} : Props) => {
                         <Grid item xs={12}>
                             <TextField
                                 required
+                                InputProps={{
+                                    readOnly: (mode === MemberMode.VIEW || mode === MemberMode.EDIT),
+                                }}
                                 id={"member-uuid"}
                                 name={"member-uuid"}
                                 label={"Member UUID"}
                                 fullWidth
                                 variant={"standard"}
-                                helperText={"Error"}
+                                helperText={"Read Only"}
                             />
                         </Grid>
                     }
                     <Grid item xs={12}>
                         <TextField
                             required
+                            InputProps={{
+                                readOnly: (mode === MemberMode.VIEW),
+                            }}
                             id={"member-name"}
                             name={"member-name"}
                             label={"Member Name"}
                             fullWidth
                             variant={"standard"}
-                            helperText={"Error"}
+                            value={newMember.name}
+                            helperText={(mode === MemberMode.VIEW) ? "Read Only" : error.nameError}
+                            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                const {value} = event.target;
+                                if (value.trim() === "") {
+                                    setError((prevState: ErrorMsgType) => {
+                                        return {...prevState, "nameError" : "Member name is required"}
+                                    });
+                                } else if (!/^[A-Za-z][A-Za-z. ]+$/.test(value)) {
+                                    setError((prevState: ErrorMsgType) => {
+                                        return {...prevState, "nameError" : "Enter valid member name"}
+                                    })
+                                } else {
+                                    setError((prevState: ErrorMsgType) => {
+                                        return {...prevState, "nameError": " "}
+                                    })
+                                }
+                                setNewMember((prevState: Member) => {
+                                    return {...prevState, "name" : value}
+                                })
+                            }}
                         />
                     </Grid>
                     <Grid item xs={12}>
                         <TextField
                             required
+                            InputProps={{
+                                readOnly: (mode === MemberMode.VIEW),
+                            }}
                             id={"member-address"}
                             name={"member-address"}
                             label={"Member Address"}
                             fullWidth
                             variant={"standard"}
-                            helperText={"Error"}
+                            value={newMember.address}
+                            helperText={(mode === MemberMode.VIEW) ? "Read Only" : error.addressError}
+                            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                const {value} = event.target;
+                                if (value.trim() === "") {
+                                    setError((prevState: ErrorMsgType) => {
+                                        return {...prevState, "addressError" : "Member address is required"}
+                                    });
+                                } else if (!/^[A-Za-z\d][A-Za-z\d-|/# ,.:;\\]+$/.test(value)) {
+                                    setError((prevState: ErrorMsgType) => {
+                                        return {...prevState, "addressError" : "Enter valid member address"}
+                                    })
+                                } else {
+                                    setError((prevState: ErrorMsgType) => {
+                                        return {...prevState, "addressError": " "}
+                                    })
+                                }
+                                setNewMember((prevState: Member) => {
+                                    return {...prevState, "address" : value}
+                                })
+                            }}
                         />
                     </Grid>
                     <Grid item xs={12}>
                         <TextField
                             required
+                            InputProps={{
+                                readOnly: (mode === MemberMode.VIEW),
+                            }}
                             id={"member-contact"}
                             name={"member-contact"}
                             label={"Member Contact"}
                             fullWidth
                             variant={"standard"}
-                            helperText={"Error"}
+                            value={newMember.contact}
+                            helperText={(mode === MemberMode.VIEW) ? "Read Only" : error.contactError}
+                            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                const {value} = event.target;
+                                if (value.trim() === "") {
+                                    setError((prevState: ErrorMsgType) => {
+                                        return {...prevState, "contactError" : "Member contact number is required"}
+                                    });
+                                } else if (!/^\d{3}-\d{7}$/.test(value)) {
+                                    setError((prevState: ErrorMsgType) => {
+                                        return {...prevState, "contactError" : "Enter valid member contact number"}
+                                    })
+                                } else {
+                                    setError((prevState: ErrorMsgType) => {
+                                        return {...prevState, "contactError": " "}
+                                    })
+                                }
+                                setNewMember((prevState: Member) => {
+                                    return {...prevState, "contact" : value}
+                                })
+                            }}
                         />
                     </Grid>
                 </Grid>
