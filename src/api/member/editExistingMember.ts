ff19055a772881a1e11a7d0303d13be9b9e29237
Pubudu Janith <pubudujanith123@gmail.com>
2023-09-17 17:37:26 +0530

    Update editExistingMember api point method type

--- src/api/member/editExistingMember.ts
+++ src/api/member/editExistingMember.ts
@@ -3,5 +3,5 @@ import {responseHandler} from "../util/responseHandler";
 import {Member} from "../../components/CreateEditViewMember";
 
 export const editExistingMember = async (memberUUID: string, body: Member) => {
-    return responseHandler(await requestApi(METHODS.POST, URL.EDIT_EXISTING_MEMBER(memberUUID), body));
+    return responseHandler(await requestApi(METHODS.PATCH, URL.EDIT_EXISTING_MEMBER(memberUUID), body));
 }
